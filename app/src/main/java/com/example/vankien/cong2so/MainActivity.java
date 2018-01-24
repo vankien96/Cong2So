f3f66f8b61a357a8a89e717f6d7c8b4daf497235
Trương Văn Kiên <vankien@Truongs-MacBook-Pro.local>
2018-01-24 22:15:26 +0700

    first commit

---
+++ app/src/main/java/com/example/vankien/cong2so/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.vankien.cong2so;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.QuickContactBadge;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity{
+    TextView txtKetQua;
+    EditText txtSoA,txtSoB;
+    Button btnTong;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        addControls();
+        addEvents();
+    }
+
+    private void addEvents() {
+        btnTong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int soA = Integer.parseInt(txtSoA.getText().toString());
+                int soB = Integer.parseInt(txtSoB.getText().toString());
+                int tong = soA+soB;
+                txtKetQua.setText(""+tong);
+            }
+        });
+    }
+
+    private void addControls() {
+        txtKetQua = findViewById(R.id.txtKetQua);
+        btnTong = findViewById(R.id.btnTong);
+        txtSoA = findViewById(R.id.txtSoA);
+        txtSoB = findViewById(R.id.txtSoB);
+    }
+}
